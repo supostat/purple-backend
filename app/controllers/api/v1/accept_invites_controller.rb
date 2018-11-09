ee730c3920b9fa0767b3de6d687a5cdb561f803a
Igor Pugachev <ipugachev84@gmail.com>
2018-11-09 13:58:01 +0000

    Add AcceptInvitesController

---
+++ app/controllers/api/v1/accept_invites_controller.rb
@@ -0,0 +1,30 @@
+class Api::V1::AcceptInvitesController < ApplicationController
+  def index
+    result = AcceptInvitePageData.new(invitation_token: invitation_token_from_params).all
+    if result.success?
+      render json: {
+        invitedUser: Api::V1::AcceptInvites::UserSerializer.new(result.user),
+        base64Png: result.base64Png,
+      }
+    else
+      raise "qweqwe"
+    end
+  end
+
+  def accept
+    invitation_token = params.fetch(:invitationToken)
+    user = User.find_by_invitation_token(invitation_token, true)
+    if user.present? && user.invited_to_sign_up? && user.created_by_invite?
+      user = AcceptInvite.new(user: user).call(params: params)
+      sign_in(user)
+    else
+      render json: {erros: "error"}, status: 422
+    end
+  end
+
+  private
+
+  def invitation_token_from_params
+    params.fetch(:invitationToken)
+  end
+end
