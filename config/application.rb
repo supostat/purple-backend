dac131fd0a2eb08dfa13e943001f49fdb6ae4a3e
Igor Pugachev <ipugachev84@gmail.com>
2018-11-12 08:35:21 +0000

    Add `page_data` to autoload_paths

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,7 @@ module Purple
     config.load_defaults 5.2
     config.autoload_paths += Dir["#{config.root}/app/services/**/"]
     config.autoload_paths += Dir["#{config.root}/app/serializers/**/"]
+    config.autoload_paths += Dir["#{config.root}/app/page_data/**/"]
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
