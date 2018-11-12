df4b84f8b089ae93fc37393e99416caae50186b9
Igor Pugachev <ipugachev84@gmail.com>
2018-11-12 09:43:11 +0000

    Add roles and role name validation

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,6 +1,7 @@
 class Role < ApplicationRecord
 has_and_belongs_to_many :users, :join_table => :users_roles
 
+ROLES = [:admin, :user]
 
 belongs_to :resource,
            :polymorphic => true,
@@ -11,5 +12,7 @@ validates :resource_type,
           :inclusion => { :in => Rolify.resource_types },
           :allow_nil => true
 
+validates :name,
+          :inclusion => { :in => ROLES}
 scopify
 end
