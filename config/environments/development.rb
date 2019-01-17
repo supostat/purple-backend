32c1078b12606a1053efc8cf964d35dd411ae0ed
Igor Pugachev <ipugachev84@gmail.com>
2019-01-17 14:08:31 +0000

    Add MailTrap for production

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,12 +14,12 @@ Rails.application.configure do
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
-  if Rails.root.join('tmp', 'caching-dev.txt').exist?
+  if Rails.root.join("tmp", "caching-dev.txt").exist?
     config.action_controller.perform_caching = true
 
     config.cache_store = :memory_store
     config.public_file_server.headers = {
-      'Cache-Control' => "public, max-age=#{2.days.to_i}"
+      "Cache-Control" => "public, max-age=#{2.days.to_i}",
     }
   else
     config.action_controller.perform_caching = false
@@ -44,7 +44,6 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
-
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
@@ -52,10 +51,21 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.action_mailer.default_url_options = {
-    host: ENV['HTTP_HOST'] || 'localhost'
-  }
+  # mail catcher
+  # config.action_mailer.default_url_options = {
+  #   host: ENV['HTTP_HOST'] || 'localhost'
+  # }
 
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = { :address => "mailcatcher", :port => 1025 }
+  config.action_mailer.default_url_options = { :host => 'localhost:8080' }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { :address => "mailcatcher", :port => 1025 }
+  config.action_mailer.smtp_settings = {
+    :user_name => "42a61b3469a4c4",
+    :password => "bfc00d2a2f23e6",
+    :address => "smtp.mailtrap.io",
+    :domain => "smtp.mailtrap.io",
+    :port => "2525",
+    :authentication => :cram_md5,
+  }
 end
