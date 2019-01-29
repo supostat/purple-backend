349255baf9cf52f746b77b985bef4235e55b26d8
Igor Pugachev <ipugachev84@gmail.com>
2019-01-29 14:08:54 +0000

    Add UserProfile serializers

---
+++ app/serializers/api/v1/user_profile/user_serializer.rb
@@ -0,0 +1,35 @@
+class Api::V1::UserProfile::UserSerializer < ActiveModel::Serializer
+  attributes \
+    :id,
+    :email,
+    :role,
+    :invitedAt,
+    :venuesIds,
+    :firstName,
+    :surname,
+    :status
+
+  def invitedAt
+    object.invitation_sent_at
+  end
+
+  def venuesIds
+    object.work_venues.pluck(:id)
+  end
+
+  def role
+    object.roles_name[0]
+  end
+
+  def firstName
+    object.first_name
+  end
+
+  def surname
+    object.surname
+  end
+
+  def status
+    object.current_state
+  end
+end
---
+++ app/serializers/api/v1/user_profile/venue_serializer.rb
@@ -0,0 +1,5 @@
+class Api::V1::UserProfile::VenueSerializer < ActiveModel::Serializer
+  attributes \
+    :id,
+    :name
+end
