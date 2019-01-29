b2010eefc7c63fab9caf942ea4093c7309c3b6f1
Igor Pugachev <ipugachev84@gmail.com>
2019-01-29 19:45:08 +0000

    Add page normalization to Pagination service

--- app/services/pagination.rb
+++ app/services/pagination.rb
@@ -6,7 +6,8 @@ class Pagination
   end
 
   def for_load_more
-    pagy = Pagy.new(count: records.count, page: current_page, items: per_page, circle: true)
+    count = records.count
+    pagy = Pagy.new(count: count, page: normalize_current_page(count, current_page, per_page), items: per_page)
     showing = pagy.offset + pagy.items
     {
       records: records.limit(showing),
@@ -16,11 +17,23 @@ class Pagination
         next: pagy.next,
         page: pagy.page,
         pagy: pagy,
+        norm: normalize_current_page(count, current_page, per_page),
       },
     }
   end
 
   private
+
+  def normalize_current_page(count, current_page, per_page)
+    page = current_page.to_i rescue 1
+    return 1 if page == 0 || count == 0
+    max_pages = count.fdiv(per_page).ceil
+    if page > max_pages
+      max_pages
+    else
+      page
+    end
+  end
 
   attr_reader :records, :per_page, :current_page
 end
