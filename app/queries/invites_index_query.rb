09129c2992ff20ce6acbe1f349995cea32980448
Igor Pugachev <ipugachev84@gmail.com>
2019-01-29 12:41:04 +0000

    Add InvitesIndexQuery service

---
+++ app/queries/invites_index_query.rb
@@ -0,0 +1,35 @@
+class InvitesIndexQuery
+  def initialize(params:)
+    @params = params
+  end
+
+  def all
+    query = User.created_by_invite
+    if params[:venues].present?
+      query = query.joins(:work_venues).where(users_venues: {venue_id: params[:venues]}).distinct
+    end
+    if params[:role].present?
+      query = query.with_role(params[:role])
+    end
+    if params[:email].present?
+      query = query.search_email(params[:email])
+    end
+    if params[:status].present?
+      case params[:status]
+      when User::INVITATION_REVOKED_STATUS
+        query = query.where(invitation_token: nil).where.not(invitation_revoked_at: nil)
+      when User::INVITATION_PENDING_STATUS
+        query = query.invitation_not_accepted
+      when User::INVITATION_ACCEPTED_STATUS
+        query = query.invitation_accepted
+      else
+        raise "Unknown user status in query param"
+      end
+    end
+    query
+  end
+
+  private
+
+  attr_reader :params
+end
