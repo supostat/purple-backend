8e2df88ba27d7e49dad4dea12f64ef054ecf5f42
William Billington <william.billington@gmail.com>
2019-01-31 13:53:13 +0000

    Routes whitespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,36 +1,41 @@
 Rails.application.routes.draw do
   default_url_options :host => ENV["HTTP_HOST"] || "localhost:8080"
   devise_for :users,
-             skip: [:passwords],
-             defaults: {format: :json},
-             path: "",
-             path_names: {
-               sign_in: "login",
-               sign_out: "logout",
-               registration: "signup",
-             },
-             controllers: {
-               sessions: "sessions",
-               registrations: "registrations",
-             }
+    skip: [:passwords],
+    defaults: {format: :json},
+    path: "",
+    path_names: {
+      sign_in: "login",
+      sign_out: "logout",
+      registration: "signup",
+    },
+    controllers: {
+      sessions: "sessions",
+      registrations: "registrations",
+    }
+
   as :user do
     get "reset-password", :to => "devise/passwords#edit"
   end
+
   namespace :api do
     namespace :v1 do
       resources :tests, only: [:index]
       resources :invites, only: [:index, :create]
+
       resource :passwords, only: [:index] do
         collection do
           post :send_reset_password_email
           post :reset_password
         end
       end
+
       resources :users, only: [:index, :show] do
         member do
           get :history
         end
       end
+
       resources :accept_invites, only: [:index] do
         collection do
           post :accept
