ba89ee5b02e0c69e5f091b0a3303373d4523fab4
Igor Pugachev <ipugachev84@gmail.com>
2019-02-01 09:23:42 +0000

    Add error handler to SendResetPasswordEmail

--- app/services/send_reset_password_email.rb
+++ app/services/send_reset_password_email.rb
@@ -1,15 +1,31 @@
 class SendResetPasswordEmail
+  Result = Struct.new(:success, :api_errors) do
+    def success?
+      success
+    end
+  end
+
   def initialize(email:)
     @email = email
   end
 
   def call
-    user = User.find_by(email: email)
-    if user.present?
+    user = User.find_or_initialize_by({email: email})
+
+    if user.persisted?
       user.send_reset_password_instructions
     else
+      if email.blank?
+        user.errors.add(:email, :blank)
+      end
       # do nothing
     end
+    api_errors = nil
+    success = user.errors.empty?
+    unless success
+      api_errors = SendResetPasswordEmailApiErrors.new(user: user)
+    end
+    Result.new(success, api_errors)
   end
 
   private
