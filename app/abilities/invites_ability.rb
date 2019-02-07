be6d3733b3d84bcf906ae58c8d38933990ea6b92
Igor Pugachev <ipugachev84@gmail.com>
2019-02-07 13:29:20 +0000

    Update invites abilities

--- app/abilities/invites_ability.rb
+++ app/abilities/invites_ability.rb
@@ -2,7 +2,7 @@ class InvitesAbility
   include CanCan::Ability
 
   def initialize(user)
-    return unless user.has_role? :manager
+    return unless user.has_any_role?(:manager, :admin)
     can :manage, User
   end
 end
