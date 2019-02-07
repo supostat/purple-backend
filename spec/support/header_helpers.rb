a990d7ee887c51b90d4b3aeb0bf7461ba9192880
Igor Pugachev <ipugachev84@gmail.com>
2019-02-07 13:37:48 +0000

    Add set_controller_spec_headers to espec headers helper

--- spec/support/header_helpers.rb
+++ spec/support/header_helpers.rb
@@ -4,4 +4,9 @@ module HeaderHelpers
     header_key = 'Authorization'
     header(header_key, header_data.fetch(header_key))
   end
+
+  def set_controller_spec_headers(user)
+    headers = { 'Accept' => 'application/json', 'Content-Type' => 'application/json' }
+    request.headers.merge! Devise::JWT::TestHelpers.auth_headers(headers, user)
+  end
 end
