c6fc4bab0828ac8fa3998bb10306f6fbcae64f01
Igor Pugachev <ipugachev84@gmail.com>
2019-02-07 13:26:24 +0000

    Change JWT token expiration from 1 day to 30 days

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -33,7 +33,7 @@ Devise.setup do |config|
     jwt.revocation_requests = [
       ['DELETE', %r{^/logout$}]
     ]
-    jwt.expiration_time = 1.day.to_i
+    jwt.expiration_time = 30.day.to_i
   end
 
   # Configure the class responsible to send e-mails.
