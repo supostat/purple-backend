9f2abca5e9421815f859cc09b398d43153f3fb77
William Billington <william.billington@gmail.com>
2019-02-07 11:23:06 +0000

    Add specs for every factory

--- spec/factory_spec.rb
+++ spec/factory_spec.rb
@@ -3,7 +3,26 @@ require 'rails_helper'
 RSpec.describe "Factories" do
   describe "user factory" do
     it "should be valid" do
-      expect(FactoryBot.create(:user)).to be_persisted
+      created_user = FactoryBot.create(:user)
+      expect(created_user).to be_persisted
+    end
+
+    it 'should not register as invited' do
+      created_user = FactoryBot.create(:user)
+      expect(created_user.created_by_invite?).to eq(false)
+    end
+
+    describe 'invited_trait' do
+      it 'should be valid' do
+        created_user = FactoryBot.create(:user, :invited)
+        expect(created_user).to be_persisted
+      end
+
+      it 'should report itself as invited' do
+        created_user = FactoryBot.create(:user, :invited)
+        expect(created_user.created_by_invite?).to eq(true)
+        expect(created_user.invited_to_sign_up?).to eq(true)
+      end
     end
   end
 
