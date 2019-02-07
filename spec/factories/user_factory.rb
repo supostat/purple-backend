ec7602be445b94068e6d5ab9d8d73d7a910e73dd
Igor Pugachev <ipugachev84@gmail.com>
2019-02-07 13:38:10 +0000

    Add traits to user factory

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -15,5 +15,24 @@ FactoryBot.define do
       _role_model = Role.find_or_create_by(name: _role)
       user.roles = [_role_model]
     end
+
+    trait :invited do
+      password { nil }
+      password_confirmation { nil }
+      invitation_created_at { Time.current }
+      invitation_sent_at { Time.current }
+    end
+
+    trait :admin do
+      after(:create) {|user| user.add_role(Role::ADMIN_ROLE)}
+    end
+
+    trait :manager do
+      after(:create) {|user| user.add_role(Role::MANAGER_ROLE)}
+    end
+
+    trait :disabled do
+      after(:create) {|user| user.update({disabled_at: Time.current, disabled_by_user: FactoryBot.create(:user)})}
+    end
   end
 end
