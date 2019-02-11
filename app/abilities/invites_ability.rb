fb22db08af708d6a5878fbb812531fdbde16e1cd
Igor Pugachev <ipugachev84@gmail.com>
2019-02-11 13:47:41 +0000

    Update InvitesAbility

--- app/abilities/invites_ability.rb
+++ app/abilities/invites_ability.rb
@@ -2,18 +2,13 @@ class InvitesAbility
   include CanCan::Ability
 
   def initialize(user)
-    return unless user.has_any_role?(Role::MANAGER_ROLE, Role::ADMIN_ROLE)
+    return unless user.has_any_role?(Role::ADMIN_ROLE, Role::MANAGER_ROLE)
 
-    can :view, :invites_index do
-      user.has_role? Role::ADMIN_ROLE
-    end
+    can :show, User
+    can :revoke, User
 
-    can :create_invites do
-      user.has_role? Role::ADMIN_ROLE
-    end
-
-    can :revoke_invite, User do |invite_user|
-      user.has_role? Role::ADMIN_ROLE
+    if user.has_role? Role::ADMIN_ROLE
+      can :create, User
     end
   end
 end
