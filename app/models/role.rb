c8213be90588251bdb152bf8a011c4aaddb2a40b
William Billington <william.billington@gmail.com>
2019-02-11 18:53:00 +0000

    whitespace tweak

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,25 +1,25 @@
 class Role < ApplicationRecord
-has_and_belongs_to_many :users, :join_table => :users_roles
+  has_and_belongs_to_many :users, :join_table => :users_roles
 
-ADMIN_ROLE = "admin"
-MANAGER_ROLE = "manager"
+  ADMIN_ROLE = "admin"
+  MANAGER_ROLE = "manager"
 
-ROLES = [ADMIN_ROLE, MANAGER_ROLE]
+  ROLES = [ADMIN_ROLE, MANAGER_ROLE]
 
-ROLES_TITLES = {
-  ADMIN_ROLE => "Admin",
-  MANAGER_ROLE => "Manager",
-}
+  ROLES_TITLES = {
+    ADMIN_ROLE => "Admin",
+    MANAGER_ROLE => "Manager",
+  }
 
-belongs_to :resource,
-           :polymorphic => true,
-           :optional => true
+  belongs_to :resource,
+             :polymorphic => true,
+             :optional => true
 
 
-validates :resource_type,
-          :inclusion => { :in => Rolify.resource_types },
-          :allow_nil => true
+  validates :resource_type,
+            :inclusion => { :in => Rolify.resource_types },
+            :allow_nil => true
 
-validates :name, presence: true, :inclusion => { :in => ROLES }
-scopify
+  validates :name, presence: true, :inclusion => { :in => ROLES }
+  scopify
 end
