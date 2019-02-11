6fb1feb5315a0fac7de5c3a857e4c4bc98a28aa2
Igor Pugachev <ipugachev84@gmail.com>
2019-02-11 13:54:54 +0000

    Add UsersIndexQuery and statuses to UsersPageData

--- app/page_data/users_page_data.rb
+++ app/page_data/users_page_data.rb
@@ -1,15 +1,24 @@
 class UsersPageData
-  Result = Struct.new(:success, :users, :roles, :venues) do
+  Result = Struct.new(:success, :users, :roles, :venues, :statuses) do
     def success?
       success
     end
   end
 
+  def initialize(params:)
+    @params = params
+  end
+
   def all
     success = true
-    users = User.created_by_invite
-    roles = Role::ROLES
+    users = UsersIndexQuery.new(params: params).all
+    roles = Role::ROLES_TITLES
     venues = Venue.all
-    Result.new(success, users, roles, venues)
+    statuses = User::STATUSES_TEXT
+    Result.new(success, users, roles, venues, statuses)
   end
+
+  private
+
+  attr_reader :params
 end
